61782b1711d622edb6cc6c2492de397411175642
anil <anil@Basu>
2020-01-12 10:13:11 +0530

    design

---
+++ src/pqr/Cool1.java
@@ -0,0 +1,11 @@
+package pqr;
+
+import org.testng.annotations.Test;
+
+public class Cool1 {
+   @Test
+   public void test()
+   {
+	   System.out.println("hi");
+   }
+}
---
+++ src/pqr/Cool2.java
@@ -0,0 +1,10 @@
+package pqr;
+
+import org.testng.annotations.Test;
+
+public class Cool2 {
+	@Test
+	public void test2(){
+		System.out.println("hello");
+	}
+}
